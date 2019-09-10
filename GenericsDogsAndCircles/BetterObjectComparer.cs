e0cc6197b07cefd2eddb661d6ec1c050defe39be
BuildTools <unconfigured@null.spigotmc.org>
2019-09-10 19:54:39 +0200

    Add project files.

---
+++ GenericsDogsAndCircles/BetterObjectComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDogsAndCircles
+{
+    public class BetterObjectComparer<T> where T : IComparable<T>
+    {
+        public T Largest(T a, T b, T c)
+        {
+            if (a.CompareTo(a) > b.CompareTo(c)) return a;
+            else if (b.CompareTo(b) > c.CompareTo(a)) return b;
+            else return c;
+        }
+
+    }
+}
